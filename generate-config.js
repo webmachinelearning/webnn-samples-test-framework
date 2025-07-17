a404af58fa3eaa2e42e00f6273cd1c34b10af202
Zhibo Wang <zhibo1.wang@intel.com>
2025-07-17 15:26:57 +0800

    Use retry options in rmSync()

--- generate-config.js
+++ generate-config.js
@@ -54,7 +54,7 @@ function filterSamplesWithDevices(config, devices) {
 // chrome_canary & cpu, npu, gpu by default
 const ORIGINAL_CONFIG = {
   browser: "chrome_canary",
-  browserArgs: ["--start-maximized", "--noerrdialogs"],
+  browserArgs: ["--start-maximized"],
   browserArgsWebnn: ["--enable-features=WebMachineLearningNeuralNetwork,WebNNOnnxRuntime"],
   browserUserData: true,
   browserUserDataPath: "",
